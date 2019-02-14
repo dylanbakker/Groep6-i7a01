1600ee4ab4d6bc4d8ecc14668aa41d2caea146c8
dylanbakker <36030671+dylanbakker@users.noreply.github.com>
2019-02-14 08:41:35 +0100

    Add files via upload

---
+++ Sprint9/Sprint9/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sprint9.Models;
+
+namespace Sprint9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult About(string firstName)
+        {
+            return Content($"Welkom bij SoundSharp {firstName}");
+        }
+
+        /* public IActionResult Error()
+         {
+             return View(new User { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+         }*/
+    }
+}
---
+++ Sprint9/Sprint9/Models/User.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Sprint9.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+    }
+}
