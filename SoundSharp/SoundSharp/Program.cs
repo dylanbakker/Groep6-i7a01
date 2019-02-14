250c49e079cbc6db0cd6d149e926d7e4a34a83f7
dylanbakker <36030671+dylanbakker@users.noreply.github.com>
2019-02-14 14:00:20 +0100

    Add files via upload

--- SoundSharp/SoundSharp/Program.cs
+++ SoundSharp/SoundSharp/Program.cs
@@ -8,7 +8,6 @@ namespace SoundSharp
 {
     class Program
     {
-        //make struct MP3 for storage of MP3 players
         public struct MP3
         {
             public int id;
@@ -19,76 +18,56 @@ namespace SoundSharp
             public int stock;
         }
 
-        static void Main(string[] args)
-        {
+       
 
-            Console.WriteLine("please enter your name here");
-            //read name
-            var name = Console.ReadLine();
-            //read password
-            Console.WriteLine("now please enter your password");
-            var inputPassword = Console.ReadLine();
-
-            //check if password matches and display correct response
-            string IsOk = Login(inputPassword);
-            switch (IsOk)
+            static void Main(string[] args)
             {
-                case "OK":
-                    Console.WriteLine("Welcome to soundsharp {0}", name);
-                    ShowMenu();
-                    break;
-
-                case "NotOK":
-                    Console.WriteLine("password incorrect");
-                    break;
-
-                default:
-                    Console.WriteLine("error: no valid output. please try again");
-                    break;
+                Console.WriteLine("Wat is je naam ?");
+                string name = Console.ReadLine();
+                Login(name);
             }
-        }
-        public static string Login(string input)
+            public static string Login(string name)
         {
-            //set constants for the maximum tries and the password
-            const string PASSWORD = "SHARPSOUND";
-            const int MaxTries = 3;
+            int loginAttempts = 0;
+            int poging = 1;
 
-            //set some extra variables
-            var x = 1;
-            var returnValue = "NotOK";
-
-            do
+            for (int i = 0; i < 3; i++)
             {
-                //check if imput matches password
-                if (input == PASSWORD)
+                if (poging == 3)
                 {
-                    //if password matches, break out of the loop and approve password
-                    returnValue = "OK";
-                    break;
+                    Console.WriteLine("LET OP: Laatste poging!");
+                }
+                Console.WriteLine("Poging " + poging + " van 3");
+                Console.WriteLine("Wachtwoord invoeren");
+                string PassWord = Console.ReadLine();
+
+                if (PassWord != "SHARPSOUND")
+                {
+                    loginAttempts++;
+                    poging++;
                 }
                 else
                 {
-                    //if password does not match, repeat circle till max amount of tries has been reached
-                    x++;
-                    Console.WriteLine("wachtwoord fout try " + x + " out of 3");
-
-                    if (x == 3)
-                    {
-                        Console.WriteLine("LET OP! laatste poging");
-                    }
-
-                    input = Console.ReadLine();
-
-                    //extra check if password is right
-                    if (input == PASSWORD)
-                    {
-                        returnValue = "Juist";
-                        break;
-                    }
+                    break;
                 }
+            }
 
-            } while (x != MaxTries);
-            return returnValue;
+            //Display the result
+            if (loginAttempts > 2)
+            {
+                Console.WriteLine("Wachtwoord " + Wachtwoord.onjuist);
+            }
+            else
+            {
+                Console.WriteLine(Wachtwoord.juist + ", Welkom bij SoundSharp " + name);
+                ShowMenu();
+            }
+            return Console.ReadLine();
+        }
+        enum Wachtwoord
+        {
+            juist = 0,
+            onjuist = 1,
         }
         public static void ShowMenu()
         {
@@ -101,6 +80,7 @@ namespace SoundSharp
             //loop for key imput for available options
             while (true)
             {
+
                 char keyChar = Console.ReadKey(true).KeyChar;
 
                 if ((int)keyChar == 57)
@@ -108,13 +88,82 @@ namespace SoundSharp
                     //9 button pressed, exit loop and program
                     break;
                 }
+                else if((int)keyChar == 52)
+                {
+                    ShowStatistics(mp3Array);
+                }
+                else if ((int)keyChar == 53)
+                {
+                    //5 button is pressed. add mp3
+                    //make new unique ID
+                    int newMP3Id = mp3Array.Count + 1;
+
+                    //get Brand name
+                    Console.WriteLine("Please enter the brand name of the MP3 player.");
+                    string newMP3Brand = Console.ReadLine();
+
+                    //get Model name
+                    Console.WriteLine("Please enter the Model name.");
+                    string newMP3Model = Console.ReadLine();
+
+                    //get storage amount
+                    int newMP3Storage;
+                    try
+                    {
+                        Console.WriteLine("Please enter the amount of storage in MB");
+                        newMP3Storage = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("please enter a number.");
+                        Console.ResetColor();
+                        return;
+                    }
+                    //get price
+                    double newMP3Price;
+                    try
+                    {
+                        Console.WriteLine("Please enter the price, including two decimals. even if i would be ,00");
+                        newMP3Price = Convert.ToDouble(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("please enter a number.");
+                        Console.ResetColor();
+                        return;
+                    }
+
+                    //set Stock amount
+                    int newMP3Stock = 0;
+
+                    //make a new MP3 and add it to the ArrayList
+                    MP3 newMP3 = new MP3
+                    {
+                        id = newMP3Id,
+                        brand = newMP3Brand,
+                        model = newMP3Model,
+                        storageSpace = newMP3Storage,
+                        price = newMP3Price,
+                        stock = newMP3Stock
+                    };
+
+                    mp3Array.Add(newMP3);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("MP3 player added");
+                    Console.ResetColor();
+
+                }
                 else if ((int)keyChar == 56)
                 {
+                    Console.Clear();
                     //8 button pressed, show menu again
                     ShowOptionMenu();
                 }
                 else if ((int)keyChar == 49)
                 {
+                    Console.Clear();
                     //1 button pressed, show all MP3 players by looping through them
                     foreach (MP3 i in mp3Array)
                     {
@@ -125,9 +174,13 @@ namespace SoundSharp
                         Console.WriteLine(i.storageSpace);
                         Console.WriteLine(i.price);
                     }
+                    Console.WriteLine("");
+                    Console.WriteLine("Press 8 to return back to the menu.");
+                    Console.WriteLine("Press 9 to exit the console.");
                 }
                 else if ((int)keyChar == 50)
                 {
+                    Console.Clear();
                     //2 button pressed, show available stock by looping through all of them
                     foreach (MP3 i in mp3Array)
                     {
@@ -135,11 +188,18 @@ namespace SoundSharp
                         Console.WriteLine(i.id);
                         Console.WriteLine(i.stock);
                     }
+                    Console.WriteLine("");
+                    Console.WriteLine("Press 8 to return back to the menu.");
+                    Console.WriteLine("Press 9 to exit the console.");
                 }
                 else if ((int)keyChar == 51)
                 {
+                    Console.Clear();
                     //3 button pressed, mutate stock
                     MutateStruct(mp3Array);
+                    Console.WriteLine("");
+                    Console.WriteLine("Press 8 to return back to the menu.");
+                    Console.WriteLine("Press 9 to exit the console.");
                 }
             }
             Console.WriteLine("press any key to close the program");
@@ -152,7 +212,7 @@ namespace SoundSharp
             Console.WriteLine("2: Show available stock");
             Console.WriteLine("3: Muteer stock");
             Console.WriteLine("4: test");
-            Console.WriteLine("5: test");
+            Console.WriteLine("5: Toevoegen MP3");
             Console.WriteLine("6: test");
             Console.WriteLine("7: test");
             Console.WriteLine("8: Show Menu");
@@ -176,19 +236,12 @@ namespace SoundSharp
 
                         if (inputStock >= 0)
                         {
-                            //mp3Array.
-                            //mp3Array.RemoveAt(4);
-                          //  mp3Array.Add(inputStock);
-                            selectedMP3.stock = inputStock;
+                            Console.WriteLine("De stock inhoud van {1} {2} is aangepast naar: {0}", inputStock, selectedMP3.brand, selectedMP3.model);
 
-                            foreach (MP3 y in mp3Array)
-                            {
-                                Console.WriteLine("");
-                                Console.WriteLine(y.id);
-                                Console.WriteLine(y.stock);
-                            }
 
-                        } else
+
+                        }
+                        else
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("Error: please enter a value greater then 0");
@@ -198,7 +251,8 @@ namespace SoundSharp
 
                 }
 
-            } catch
+            }
+            catch
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Error: please enter a number");
@@ -225,7 +279,7 @@ namespace SoundSharp
                 model = "XM 600",
                 storageSpace = 8192,
                 price = 224.95,
-                stock = 0,
+                stock = 500
             };
 
             MP3 third = new MP3
@@ -267,6 +321,27 @@ namespace SoundSharp
             mp3Array.Add(fifthMP3);
 
             return mp3Array;
+        }
+
+        public static void ShowStatistics(ArrayList mp3Array)
+        {
+
+            int totalStock = 0;
+            double totalValue = 0;
+            ArrayList ppmb = new ArrayList();
+
+            foreach (MP3 i in mp3Array)
+            {
+                totalStock += i.stock;
+
+                totalValue += i.price * i.stock;
+            }
+
+            //calculate average price per MP3 player
+            double averagePrice = totalValue / totalStock;
+
+            Console.WriteLine("Total stock: {0}", totalStock);
+            Console.WriteLine("Total value of all stock: {0}EUR", totalValue);
         }
     }
 }
